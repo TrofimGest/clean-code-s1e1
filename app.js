d3ee046edf113efd7fd08fe84c9df24abd8efee4
TrofimGest <gubaoslika@gmail.com>
2021-11-25 15:31:16 +0300

    refactor: rename classes according to rule 3.2 in html-and-css.md

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ var editTask=function(){
   var editInput=listItem.querySelector('input[type=text]');
   var label=listItem.querySelector("label");
   var editBtn=listItem.querySelector(".edit");
-  var containsClass=listItem.classList.contains("incomplete-edit__mode");
+  var containsClass=listItem.classList.contains("incomplete-edit");
     //If class of the parent is .editmode
   if(containsClass){
 
@@ -99,7 +99,7 @@ var editTask=function(){
   }
 
   //toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  listItem.classList.toggle("incomplete-edit");
 };
 
 
